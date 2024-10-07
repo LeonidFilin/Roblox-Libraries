381bd1c00d5d9d200c9609a610acfc6d7c12db4c
LeonidFilin <leonizdev@mail.ru>
2024-10-07 20:07:53 +0600

    Update UILibrary.lua

--- UILibrary.lua
+++ UILibrary.lua
@@ -4,7 +4,7 @@ local UILibrary = {}
 
 local TweenService = game:GetService("TweenService")
 
--- Функция для плавного появления элемента
+-- fadeIn anim:
 function UILibrary.fadeIn(guiElement, duration)
     guiElement.BackgroundTransparency = 1
     local tweenInfo = TweenInfo.new(duration)
@@ -12,12 +12,30 @@ function UILibrary.fadeIn(guiElement, duration)
     tween:Play()
 end
 
--- Функция для плавного исчезновения элемента
+-- fadeOut anim:
 function UILibrary.fadeOut(guiElement, duration)
     guiElement.BackgroundTransparency = 0
     local tweenInfo = TweenInfo.new(duration)
     local tween = TweenService:Create(guiElement, tweenInfo, {BackgroundTransparency = 1})
     tween:Play()
+end
+
+-- gradient color slide animation:
+function UILibrary.slideGradient(gradient, time, color1, color2, color3) -- 1st param - "UIGradient"
+    -- create gradient from the colors:
+    gradient.Color = ColorSequence.new{
+        ColorSequenceKeypoint.new(0, color1),
+        ColorSequenceKeypoint.new(0.5, color2),
+        ColorSequenceKeypoint.new(1, color3)
+    }
+    -- get gradient position to the start:
+    gradient.Offset = Vector2.new(-1, 0)
+    
+    local gradientAnim = TweenService:Create(gradient, TweenInfo.new(time, Enum.EasingStyle.Quad, Enum.EasingDirection.In, 0),
+        -- move gradient forward
+        {Offset = Vector2.new(1,0)}
+    )
+    gradientAnim:Play()
 end
 
 return UILibrary
